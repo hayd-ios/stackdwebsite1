9a71a109947fb98cf46a1015374a6da369e9f235
hayd-ios <122998019+hayd-ios@users.noreply.github.com>
2024-10-29 15:23:48 +0000

    update

---
+++ app/app-ads.txt/page.js
@@ -0,0 +1,7 @@
+export default function AppAds() {
+    return (
+      <pre style={{ margin: 0, fontFamily: 'monospace' }}>
+        google.com, pub-1574070800789955, DIRECT, f08c47fec0942fa0
+      </pre>
+    )
+  }
