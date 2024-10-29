3a7fed8185e7e17c5c7fc73aa0501405f9fd80a1
hayd-ios <122998019+hayd-ios@users.noreply.github.com>
2024-10-29 15:25:41 +0000

    Update page.js

--- app/app-ads.txt/page.js
+++ app/app-ads.txt/page.js
@@ -1,7 +1,15 @@
-export default function AppAds() {
-    return (
-      <pre style={{ margin: 0, fontFamily: 'monospace' }}>
-        google.com, pub-1574070800789955, DIRECT, f08c47fec0942fa0
-      </pre>
-    )
+export const dynamic = 'force-static'
+
+export async function generateMetadata() {
+  return {
+    other: {
+      'content-type': 'text/plain',
+    },
   }
+}
+
+export default function AppAds() {
+  return (
+    <pre style={{ margin: 0, fontFamily: 'monospace', whiteSpace: 'pre-wrap' }}>google.com, pub-1574070800789955, DIRECT, f08c47fec0942fa0</pre>
+  )
+}
